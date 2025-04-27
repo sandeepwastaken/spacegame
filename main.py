bd55a13654733a92796f9bfa89b01de3c74d244f
Sandeep Bhami Shenoy <sandeepshenoy09@gmail.com>
2025-04-27 10:57:25 -0500

    Added code for odd or even

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world!")
+def isEven(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+num = int(input("Enter number:"))
+if isEven(num):
+    print("Even!")
+else:
+    print("Odd!")
