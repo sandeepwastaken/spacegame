60f7869acb1841c5ea0055dcbd89f7871b0bfe72
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2025-05-18 21:11:07 +0530

    Transferring all the ship logic to a dedicated ship class

--- main.py
+++ main.py
@@ -1,104 +1,62 @@
 import pygame
 import os
 import math
-import ship
+from ship import *
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-shipPos = [100, 100]
-triangleSpeed = 5
+
 lasers = []
 laserSpeed = 10
-shipAngle = 0
-angle = 0
-shipMoveDir = None
 
-def blurPos(topleft, amount, shipMoveDir):
-    bx, by = topleft
-    if shipMoveDir == "UP":
-        by += amount
-    elif shipMoveDir == "DOWN":
-        by -= amount
-    elif shipMoveDir == "RIGHT":
-        bx -= amount
-    elif shipMoveDir == "LEFT":
-        bx += amount
-    return (bx, by)
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 images_dir = os.path.join(script_dir, 'images')
 
-# Load and scale the spaceship image
-ship_path = os.path.join(images_dir, 'ship.png')
-shipImage = pygame.image.load(ship_path)
-shipImage = pygame.transform.scale(shipImage, (50, 50))  # Resize for better rotation
-shipOrig = shipImage.copy()  # Keep original image for rotation
+ship = Ship(Pos(100, 100), 5, os.path.join(images_dir, 'ship.png'))
+
 
 # Load the bullet image
-bullet_path = os.path.join(images_dir, 'bullet.png')
-bulletImage = pygame.image.load(bullet_path)
+# bullet_path = os.path.join(images_dir, 'bullet.png')
+# bulletImage = pygame.image.load(bullet_path)
 
 while running:
     # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                radians = math.radians(angle)
-                laser_dx = math.cos(radians) * laserSpeed
-                laser_dy = -math.sin(radians) * laserSpeed
-                lasers.append({'pos': [shipPos[0] + 25, shipPos[1] + 25], 'dir': (laser_dx, laser_dy), 'angle': angle})
+        # elif event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_SPACE:
+        #         radians = math.radians(angle)
+        #         laser_dx = math.cos(radians) * laserSpeed
+        #         laser_dy = -math.sin(radians) * laserSpeed
+        #         lasers.append({'pos': [shipPos[0] + 25, shipPos[1] + 25], 'dir': (laser_dx, laser_dy), 'angle': angle})
 
     # Update position
-    shipMoveDir = None
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        shipPos[0] -= triangleSpeed
-        shipMoveDir = "LEFT"
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        shipPos[0] += triangleSpeed
-        shipMoveDir = "RIGHT"
-    if keys[pygame.K_UP] or keys[pygame.K_w]:
-        shipPos[1] -= triangleSpeed
-        shipMoveDir = "UP"
-    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        shipPos[1] += triangleSpeed
-        shipMoveDir = "DOWN"
+    mouse_pos = pygame.mouse.get_pos()
+    ship.eval_input(keys, mouse_pos)
+    ship.move()
 
-    # Get mouse position and calculate angle
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    dx, dy = mouse_x - (shipPos[0] + 25), mouse_y - (shipPos[1] + 25)
-    desiredAngle = math.degrees(math.atan2(-dy, dx)) 
-    angleDiff = (desiredAngle - angle + 180) % 360 - 180
-    angle += angleDiff / 10
-
-    # Clear screen
     screen.fill(pygame.Color(20, 23, 36))
 
-    # Update lasers
-    for laser in lasers[:]:
-        x, y = laser['pos'][0], laser['pos'][1]
-        rotated_bullet = pygame.transform.rotate(bulletImage, laser['angle'])
-        new_rect = rotated_bullet.get_rect(center=(x, y))
-        screen.blit(rotated_bullet, new_rect.topleft)
-        laser['pos'][0] += laser['dir'][0]
-        laser['pos'][1] += laser['dir'][1]
-        sx, sy = pygame.display.get_window_size()
-        if x < -5 or y < -5 or x > sx+5 or y > sy+5:
-            lasers.remove(laser)
+    ship.draw(screen)
 
-    rotated_ship = pygame.transform.rotate(shipOrig, angle)
-    new_rect = rotated_ship.get_rect(center=(shipPos[0] + 25, shipPos[1] + 25))
-    rotated_ship.set_alpha(64)
-    screen.blit(rotated_ship, blurPos(new_rect.topleft, 12, shipMoveDir))
-    rotated_ship.set_alpha(128)
-    screen.blit(rotated_ship, blurPos(new_rect.topleft, 6, shipMoveDir))
-    rotated_ship.set_alpha(255)
-    screen.blit(rotated_ship, new_rect.topleft)
+    # Update lasers
+    # for laser in lasers[:]:
+    #     x, y = laser['pos'][0], laser['pos'][1]
+    #     rotated_bullet = pygame.transform.rotate(bulletImage, laser['angle'])
+    #     new_rect = rotated_bullet.get_rect(center=(x, y))
+    #     screen.blit(rotated_bullet, new_rect.topleft)
+    #     laser['pos'][0] += laser['dir'][0]
+    #     laser['pos'][1] += laser['dir'][1]
+    #     sx, sy = pygame.display.get_window_size()
+    #     if x < -5 or y < -5 or x > sx+5 or y > sy+5:
+    #         lasers.remove(laser)
+
 
 
     # Update display
--- ship.py
+++ ship.py
@@ -1,6 +1,7 @@
-import math as m
+import math
 import pygame as pygame
 import os as os
+from enum import Enum
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 images_dir = os.path.join(script_dir, 'images')
@@ -10,11 +11,76 @@ class Pos:
         self.x = x
         self.y = y
 
+class Direction(Enum):
+    NONE = 0
+    UP = 1
+    DOWN = 2
+    RIGHT = 3
+    LEFT = 4
+
 class Ship:
     def __init__(self, pos:Pos, speed:int, asset_path:str):
         self.pos:Pos = pos
         self.speed:int = speed
         self.texture = pygame.image.load(os.path.join(images_dir, asset_path)) 
-        self.texture = pygame.transform.scale(shipImage, (50, 50))
+        self.texture = pygame.transform.scale(self.texture, (50, 50))
         self.angle:float = 0
-        self.moveDirection = None
+        self.moveDirection:Direction = Direction.NONE
+
+    def eval_input(self, key, mouse):
+        self.moveDirection = Direction.NONE
+        if key[pygame.K_LEFT] or key[pygame.K_a]:
+            self.pos.x -= self.speed
+            self.moveDirection = Direction.LEFT
+        if key[pygame.K_RIGHT] or key[pygame.K_d]:
+            self.pos.x += self.speed
+            self.moveDirection = Direction.RIGHT
+        if key[pygame.K_UP] or key[pygame.K_w]:
+            self.pos.y -= self.speed
+            self.moveDirection = Direction.UP
+        if key[pygame.K_DOWN] or key[pygame.K_s]:
+            self.pos.y += self.speed
+            self.moveDirection = Direction.DOWN
+
+        mouse_x, mouse_y = mouse
+        dx, dy = mouse_x - (self.pos.x + 25), mouse_y - (self.pos.y + 25)
+        desiredAngle = math.degrees(math.atan2(-dy, dx)) 
+        angleDiff = (desiredAngle - self.angle + 180) % 360 - 180
+        self.angle += angleDiff / 10
+
+    def move(self):
+        if self.moveDirection == Direction.LEFT:
+            self.pos.x -= self.speed
+        if self.moveDirection == Direction.RIGHT:
+            self.pos.x += self.speed
+        if self.moveDirection == Direction.UP:
+            self.pos.y -= self.speed
+        if self.moveDirection == Direction.DOWN:
+            self.pos.y += self.speed
+
+
+    def draw(self, screen):
+        def blurPos(topleft, amount):
+            bx, by = topleft
+            if self.moveDirection == "UP":
+                by += amount
+            elif self.moveDirection == "DOWN":
+                by -= amount
+            elif self.moveDirection == "RIGHT":
+                bx -= amount
+            elif self.moveDirection == "LEFT":
+                bx += amount
+            return (bx, by)
+
+        rotated_ship = pygame.transform.rotate(self.texture, self.angle)
+        new_rect = rotated_ship.get_rect(center=(self.pos.x + 25, self.pos.y + 25))
+        rotated_ship.set_alpha(64)
+        screen.blit(rotated_ship, blurPos(new_rect.topleft, 12))
+        rotated_ship.set_alpha(128)
+        screen.blit(rotated_ship, blurPos(new_rect.topleft, 6))
+        rotated_ship.set_alpha(255)
+        screen.blit(rotated_ship, new_rect.topleft)
+
+
+
+
